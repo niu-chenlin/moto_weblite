5f47485fe82c1475bcd26a67f013464c83af4906
niu-chenlin <2544675996@qq.com>
2019-08-15 09:10:54 +0800

    add config

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,33 @@ import * as KoaBodyParser from 'koa-bodyparser';
 import * as Router from 'koa-router';
 import * as KoaStatic from 'koa-static';
 import * as KoaViews from 'koa-views';
+import * as nodemailer from 'nodemailer';
 
 const app = new Koa();
 const router:Router = new Router();
 
 router.get('/', async (ctx) => {
     await ctx.render('index');
+});
+router.post('/cooperation/', async (ctx) => {
+    const data = ctx.request.body;
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.qq.com',
+        secure: true,
+        port: 465,
+        auth: {
+            user: "2544675996@qq.com",
+            pass: "pkbvqmpgiikedicc"
+        }
+    });
+    transporter.sendMail({
+        from: "2544675996@qq.com",
+        to: '2544675996@qq.com',
+        subject: '商家合作',
+        html: `<div>来自${data.name}的合作申请，申请内容：${data.content}</div>`,
+        date: new Date(),
+    });
+    ctx.response.body = 'success';
 });
 app.use(KoaBodyParser());
 app.use(KoaViews('src/template', {extension: 'html', map: {html: 'ejs'}}));
--- src/static/script/index.js
+++ src/static/script/index.js
@@ -1,3 +1,5 @@
+let phone = /^1([38][0-9]|4[579]|5[0-3,5-9]|6[6]|7[0135678]|9[89])\d{8}$/;
+let email = /^[a-zA-z0-9]+@[a-zA-z0-9]+(\.[a-zA-Z0-9_-]+)+$/;
 $(document).ready(() => {
     $('#select-value').text("地点");
     $('.select').click(() => {
@@ -20,6 +22,7 @@ $(document).ready(() => {
         $('.footer').removeClass("blur");
         $('#relation-pop').fadeOut();
         $('#cooperation-pop').fadeOut();
+        $('#cooperation-auth-code-pop').fadeOut();
     });
     $('#relation').click(() => {
         $('.head').addClass("blur");
@@ -27,12 +30,66 @@ $(document).ready(() => {
         $('.footer').addClass("blur");
         $('#relation-pop').fadeIn();
     });
-
     $('#cooperation').click(() => {
         $('.head').addClass("blur");
         $('.content').addClass("blur");
         $('.footer').addClass("blur");
         $('#cooperation-pop').fadeIn();
     });
+    $('#auth-code').click(() => {
+        $('.head').addClass("blur");
+        $('.content').addClass("blur");
+        $('.footer').addClass("blur");
+        $('#cooperation-auth-code-pop').fadeIn();
+    });
+
+    $('#cooperation-name').blur(function() {
+        if($(this).val()) {
+            $(this).next().hide();
+        } else {
+            $(this).next().show();
+        }
+    });
+    $('#cooperation-phone').blur(function() {
+        if(phone.test($(this).val()) || email.test($(this).val())) {
+            $(this).next().hide();
+        } else {
+            $(this).next().show();
+        }
+    });
+    $('#cooperation-submit').click(function() {
+        if((phone.test($('#cooperation-phone').val()) || email.test($('#cooperation-phone').val())) && $('#cooperation-name').val()) {
+            $.post("/cooperation/", {
+                name: $('#cooperation-name').val(),
+                phone: $('#cooperation-phone').val(),
+                content: $('#cooperation-content').val()
+            }, function(data,status){
+                if(status === "success") {
+                    alert("发送成功！");
+                } else {
+                    alert("发送失败！");
+                }
+                $('.head').removeClass("blur");
+                $('.content').removeClass("blur");
+                $('.footer').removeClass("blur");
+                $('#cooperation-pop').fadeOut();
+            });
+        } else {
+            alert("请输入正确内容！");
+        }
+    });
+    let login_up = function() {
+        $('.head').addClass("blur");
+        $('.content').addClass("blur");
+        $('.footer').addClass("blur");
+        $('#register-pop').fadeIn();
+    };
 
 });
+let send_code = function() {
+    if(phone.test($('#re-phone').val())) {
+        $('.re-error').slideUp(500);
+    } else {
+        $('.re-error').slideDown(500);
+    }
+};
